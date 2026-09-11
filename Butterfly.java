9f27949919d1c93c803ea0f588465b7640d24991
soundhervelevan-ai <soundhervelevan@gmail.com>
2026-09-11 20:40:35 +0530

    Implement butterfly pattern printing in Butterfly.java
    Add a butterfly pattern printing program that takes user input for the number of rows and prints a butterfly shape using asterisks.

--- Butterfly.java
+++ Butterfly.java
@@ -1,1 +1,33 @@
+import java.util.*;
+public class Butterfly{
+ public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);System.out.print("Enter the Rows: ");
+   
+    int rows=sc.nextInt();
+    
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }  for(int j=1;j<(rows-i);j++){
+            System.out.print(" ");
+        }for(int j=(rows-i);j>1;j--){
+            System.out.print(" ");
+        }
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        } System.out.println("");}
+     
+    for(int i=rows;i>=1;i--){
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }  for(int j=1;j<=(rows-i);j++){
+            System.out.print(" ");
+        }for(int j=(rows-i);j>=1;j--){
+            System.out.print(" ");
+        }
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }System.out.println(" ");
 
+ }
+}}
