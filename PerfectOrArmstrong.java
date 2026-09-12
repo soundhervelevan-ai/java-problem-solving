a9fd441b7334c09533801bb5873f7c03a5ad0ae0
soundhervelevan-ai <soundhervelevan@gmail.com>
2026-09-12 13:11:46 +0530

    Refactor PerfectOrArmstrong logic and output

--- PerfectOrArmstrong.java
+++ PerfectOrArmstrong.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
-public class PerfectOrArmstrong {
+public class  PerfectOrArmstrong{
     public static void main(String[] args) {
         // Your Code goes here!
                Scanner sc=new Scanner(System.in);
                boolean isArmstrong=false;
                boolean isPerfect=false;
+               boolean isEven=true;
+               int root=0;
+               System.out.print("Enter a Number to check even,odd,Armstrond,perfect and digital roots :");
                int N=sc.nextInt();
                //Armstrong
                //to find digits
@@ -31,20 +34,50 @@ public class PerfectOrArmstrong {
                 if(sum==N){
                     isPerfect=true;
                 }
-      //conditions to detect either or
-          if(isPerfect && isArmstrong){
-            System.out.println("Both Perfect and Armstrong");
+              //even or odd
+              if((N&1)!=0){
+                isEven=false;
+              }
+              //digital root
+              if(N%9==0){
+                root=0;
+              }
+              else if(N==0){
+                root=0;
+              }
+              else{
+                root=N%9;
+              }
+              System.out.println("The Digital Root is "+ root);
+          if(isPerfect && isArmstrong && isEven){
+            System.out.print("Even,Perfect and Armstrong");
           }
-          else if(isPerfect){
-            System.out.println("Perfect ");
-          }else if(isArmstrong){
-            System.out.println("Armstrong ");
+          else if(isPerfect && isArmstrong && (!isEven)){
+            System.out.print("odd,Perfect and Armstrong");
+          }
+          else if(isPerfect && isEven){
+            System.out.print("Perfect and Even ");
+          } else if(isPerfect && (!isEven)){
+            System.out.print("Perfect and odd ");
+          }
+          else if(isArmstrong  && isEven){
+            System.out.print("Armstrong and Even");
+          }
+           else if(isArmstrong  && (!isEven)){
+            System.out.print("Armstrong and Odd");
           }
           else if(isPerfect|| isArmstrong){
-            System.out.println("Perfect or Armstrong");
+            System.out.print("Perfect or Armstrong");
           }
           else{
-            System.out.println("Nothing");
+            if(isEven){
+              System.out.print("Even");
+            }else{
+              System.out.print("Odd");
+            }
+            
           }
+           
+          
     }
 }
